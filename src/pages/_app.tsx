7a0f02e885210ad84efbf1d5d21762a0f8b09424
0xb0b1 <vcente82@gmail.com>
2021-05-19 14:39:35 -0300

    chakra theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { theme } from "../styles/theme";
+import { ChakraProvider } from "@chakra-ui/react";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
