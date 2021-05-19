34f496be597eca27d792bb316d044a2df07f4614
0xb0b1 <vcente82@gmail.com>
2021-05-19 17:01:27 -0300

    swiper css styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import { theme } from "../styles/theme";
+import { AppProps } from "next/dist/next-server/lib/router/router";
 import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
+import { theme } from "../styles/theme";
+import "../styles/custom-slide-style.scss";
+
+// swiper styling
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/scrollbar/scrollbar.min.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />;
