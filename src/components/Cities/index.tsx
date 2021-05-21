28cf6997a17dbc33c86c39407cac449b8ebb7b05
0xb0b1 <vcente82@gmail.com>
2021-05-21 13:26:50 -0300

    city information grid

--- src/components/Cities/index.tsx
+++ src/components/Cities/index.tsx
@@ -1,12 +1,43 @@
-import { Box, Flex, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import ReactCountryFlag from "react-country-flag";
 
-const Cities = () => {
+interface CitiesProps {
+  name: string;
+  countryName: string;
+  image: string;
+  countryCode: string;
+}
+
+const Cities = ({ name, countryName, image, countryCode }: CitiesProps) => {
   return (
-    <Box color="gray.700">
-      <Flex justify="center" mx="auto">
-        <Heading>Cidades +100</Heading>
+    <Box color="yellow.900" w="256px" h="245px" mx={["auto", "0"]}>
+      <Image src={image} alt={name} w="100%" h="173" objectFit="cover"></Image>
+      <Flex
+        align="center"
+        justify="space-between"
+        p={6}
+        border="1px"
+        borderColor="yellow.100"
+        borderTop="0"
+        borderBottomRadius="4"
+      >
+        <Flex direction="column">
+          <Heading>{name}</Heading>
+          <Text>{countryName}</Text>
+        </Flex>
+
+        <ReactCountryFlag
+          countryCode={countryCode}
+          arial-label={countryName}
+          svg
+          style={{
+            fontSize: "2em",
+            lineHeight: "2em",
+            borderRadius: "50%",
+            objectFit: "cover",
+          }}
+        />
       </Flex>
-      <SimpleGrid columns={2} spacing={2}></SimpleGrid>
     </Box>
   );
 };
