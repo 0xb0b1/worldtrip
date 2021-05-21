f1c66206f1d2fedaf0d64827185faffeaed31e85
0xb0b1 <vcente82@gmail.com>
2021-05-21 09:15:54 -0300

    slider continent data

--- src/components/SwiperContainer.tsx
+++ src/components/SwiperContainer.tsx
@@ -9,48 +9,11 @@ import SwiperCore, {
 } from "swiper/core";
 import SwiperContent from "../components/SwiperContent";
 
+import { continents } from "./data/continentSlider";
+
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const SwiperContainer = () => {
-  const continents = [
-    {
-      image: "/images/continents/america-do-norte.jpg",
-      name: "América do Norte",
-      slug: "america-do-norte",
-      description: "O continente mais tecnologico",
-    },
-    {
-      image: "/images/continents/america-do-sul.jpg",
-      name: "América do Sul",
-      slug: "america-do-sul",
-      description: "O melhor continente",
-    },
-    {
-      image: "/images/continents/asia.jpg",
-      name: "Ásia",
-      slug: "asia",
-      description: "O continente mais populoso",
-    },
-    {
-      image: "/images/continents/africa.jpg",
-      name: "África",
-      slug: "africa",
-      description: "O continente mais colorido",
-    },
-    {
-      image: "/images/continents/europe.png",
-      name: "Europa",
-      slug: "europa",
-      description: "O continente mais antigo",
-    },
-    {
-      image: "/images/continents/oceania.jpg",
-      name: "Oceania",
-      slug: "oceania",
-      description: "O continente mais peculiar",
-    },
-  ];
-
   return (
     <Box mx="5" mt="20">
       <Heading as="h2" color="gray.500" fontWeight="500" textAlign="center">
---
+++ src/components/data/continentSlider.ts
@@ -0,0 +1,38 @@
+export const continents = [
+  {
+    image: "/images/continents/america-do-norte.jpg",
+    name: "América do Norte",
+    slug: "america-do-norte",
+    description: "O continente mais tecnologico",
+  },
+  {
+    image: "/images/continents/america-do-sul.jpg",
+    name: "América do Sul",
+    slug: "america-do-sul",
+    description: "O melhor continente",
+  },
+  {
+    image: "/images/continents/asia.jpg",
+    name: "Ásia",
+    slug: "asia",
+    description: "O continente mais populoso",
+  },
+  {
+    image: "/images/continents/africa.jpg",
+    name: "África",
+    slug: "africa",
+    description: "O continente mais colorido",
+  },
+  {
+    image: "/images/continents/europe.png",
+    name: "Europa",
+    slug: "europa",
+    description: "O continente mais antigo",
+  },
+  {
+    image: "/images/continents/oceania.jpg",
+    name: "Oceania",
+    slug: "oceania",
+    description: "O continente mais peculiar",
+  },
+];
